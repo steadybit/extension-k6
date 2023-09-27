9d4e8a0ab16530f4e7c5d77ea5dfc44bf55d5694
Daniel Reuter <daniel@steadybit.com>
2023-09-27 14:06:35 +0200

    feat: add endpoints for pprof

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/steadybit/extension-kit/exthttp"
 	"github.com/steadybit/extension-kit/extlogging"
 	"github.com/steadybit/extension-kit/extruntime"
+	_ "net/http/pprof" //allow pprof
 )
 
 func main() {
